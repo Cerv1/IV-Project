1427c14c8ba6202e699fae2429782bbe8011cf2b
Adrian Morente <adrian95morente@gmail.com>
2017-11-09 12:16:16 +0100

    testeando / y /status

--- test/server_test.js
+++ test/server_test.js
@@ -2,17 +2,29 @@ var request = require('supertest'),
     app = require('../app.js');
 
 describe('Server stuff', function() {
+
+  // testing the correct status of the API
   it('Should create server', function(done) {
     request(app)
       .get('/')
-      .expect(200, done)
+      .expect('Content-Type', "text/html; charset=utf-8")
+      .expect(200, done);
+  });
+
+  // testing the correct status of the API
+  it('Should create server', function(done) {
+    request(app)
+      .get('/status')
+      .expect('Content-Type', "text/html; charset=utf-8")
+      .expect('{"status":"OK"}')
+      .expect(200, done);
   });
 
   it('Should get users info in JSON', function(done) {
     request(app)
       .get('/users')
       .expect('Content-Type', /json/)
-      .expect(200, done)
+      .expect(200, done);
   });
 
   it('Should get new user JSON', function(done) {
