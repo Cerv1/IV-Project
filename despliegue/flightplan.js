6548e831790e5d5c1f1368a0e1b15013d633b5cc
Cervi <cervick13@gmail.com>
2017-12-21 11:19:51 +0100

    Cambiando estructura

---
+++ despliegue/flightplan.js
@@ -0,0 +1,23 @@
+  var plan = require('flightplan');
+
+  // configuration only in staging machine
+  plan.target('staging', {
+    host: 'delicate-sunset-33.westus.cloudapp.azure.com',
+    username: 'vagrant',
+    agent: process.env.SSH_AUTH_SOCK
+  });
+
+  plan.remote(function(remote) {
+    remote.log('Executing aplication');
+
+    remote.with('cd ~/IV-Application', function() {
+      remote.exec('npm install');
+      remote.exec('npm install');
+      remote.exec('sudo pm2 start app.js');
+    });
+
+  });
+
+  plan.local(function(local) {
+    local.log('Clean up');
+  });
